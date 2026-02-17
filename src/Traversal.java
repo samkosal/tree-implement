ff6855f78f4307f753704134febfd3ddebb3a611
samkosal <67755908+samkosal@users.noreply.github.com>
2026-02-17 14:37:05 -0800

    finished

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,25 +1,64 @@
+import static org.junit.jupiter.api.DynamicTest.stream;
+
 public class Traversal {
   public static void main(String[] args) {
-    TreeNode root = new TreeNode(10, null, null);
-    root.left = new TreeNode(9, null, null);
-    root.left.left = new TreeNode(5, null, null);
-    root.left.right = new TreeNode(2, null, null);
+    TreeNode<Integer> root = new TreeNode<>(10, null, null);
+    root.left = new TreeNode<Integer>(9, null, null);
+    root.left.left = new TreeNode<Integer>(5, null, null);
+    root.left.right = new TreeNode<Integer>(2, null, null);
 
-    root.right = new TreeNode(15, null, null);
-    root.right.left = new TreeNode(-3, null, null);
-    root.right.right = new TreeNode(-3, null, null);
-    root.right.right.right = new TreeNode(-3, null, null);
-    System.out.println(root.value);
-    System.out.println(root.left.value);
+    root.right = new TreeNode<Integer>(15, null, null);
+    root.right.left = new TreeNode<Integer>(-3, null, null);
+    root.right.right = new TreeNode<Integer>(5, null, null);
+    root.right.right.right = new TreeNode<Integer>(22, null, null);
 
+    TreeNode<String> stringRoot = new TreeNode<String>("hello", null, null);
+    stringRoot.left = new TreeNode<String>("how", null, null);
+    stringRoot.left.left = new TreeNode<String>("are", null, null);
+    stringRoot.left.right = new TreeNode<String>("you", null, null);
+
+    stringRoot.right = new TreeNode<String>("I", null, null);
+    stringRoot.right.left = new TreeNode<String>("am", null, null);
+    stringRoot.right.right = new TreeNode<String>("so", null, null);
+    stringRoot.right.right.right = new TreeNode<String>("good", null, null);
 
     // preorder(root);
     // postorder(root);
-    inorder(root);
+    // inorder(stringRoot);
+    // printGreater(root, 7);
+    System.out.println(countNodes(stringRoot));
+  }
+
+  public static int countNodes(TreeNode<?> node) {
+    if (node == null) {
+      return 0;
+    }
+    int leftCount = countNodes(node.left);
+    int rightCount = countNodes(node.right);
+    int overallCount = leftCount + rightCount + 1;
+
+    return overallCount;
+    //return node == null ? 0 : 1 + countNodes(node.left) + countNodes(node.right);
+    // return countNodes(node.left) + countNodes(node.right) + 1;
+  }
+
+  public static void printGreater(TreeNode<Integer> node, int threshold) {
+    if (node == null) {
+      return;
+    }
+    //print value
+    if (node.value > threshold) {
+      System.out.println(node.value);
+    }
+    
+    //traverse left
+    printGreater(node.left, threshold);
+    //traverse right
+    printGreater(node.right, threshold);
   }
 
   //print a tree roted at the given node in pre-order
-  public static void preorder(TreeNode node) {
+  public static <T> void preorder(TreeNode<?> node) {
     if (node == null) {
       return;
     }
@@ -32,14 +71,14 @@ public class Traversal {
 
   }
 
-  public static void postorder(TreeNode node) {
+  public static <T> void postorder(TreeNode<?> node) {
     if (node == null) return;
     postorder(node.left);
     postorder(node.right);
     System.out.println(node.value);
   }
 
-  public static void inorder(TreeNode node) {
+  public static <T> void inorder(TreeNode<?> node) {
     if (node == null) return;
     inorder(node.left);
     System.out.println(node.value);
